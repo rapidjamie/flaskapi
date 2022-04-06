9b1da1a171cf8a9a6729f30468382858a63b9990
Jeff Ding <jeffding@Jeffs-MacBook-Pro.local>
2022-04-06 13:53:03 -0400

    fix

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 import flask
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask import FlaskPlugin
 from flask import request, jsonify
 import re
 
-from marshmallow import Schema, fields
-
-app = flask.Flask(__name__, template_folder='swagger/templates')
+app = flask.Flask(__name__)
 
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
-spec = APISpec(
-    title='flask-api-non-gateway',
-    version='1.0.0',
-    openapi_version='3.0.2',
-    plugins=[FlaskPlugin(), MarshmallowPlugin()]
-)
 
 
 @app.route('/', methods=['GET'])
@@ -24,34 +12,8 @@ def home():
     return 'hello world'
 
 
-@app.route('/api/swagger.json', methods=['GET'])
-def createSwaggerSpec():
-    return jsonify(spec.to_dict())
-
-
-class NameResponseSchema(Schema):
-    id = fields.Int()
-    name = fields.Str()
-    email = fields.Str()
-
-
-class NameListResponseSchema(Schema):
-    name_list = fields.List(fields.Nested)
-
-
 @app.route('/getAllNames')
 def index():
-    """Get List of Names in System
-    ---
-    get:
-        description: Get List of Names in System
-        responses:
-            200:
-                description: Return a name list
-                content:
-                    application/json:
-                        schema: NameListResponseSchema
-    """
     dummy_data = [{
         'id': 1,
         'name': 'Jeff Ding',
@@ -62,7 +24,7 @@ def index():
         'email': 'jeff@rapidapi.com'
     }]
 
-    return NameListResponseSchema().dump({'name_list': dummy_data})
+    return jsonify(dummy_data)
 
 
 @app.route('/validateEmail/<email>')
