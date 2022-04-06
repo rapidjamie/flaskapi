501659828353e76fb88352d1ea474f22d0898b5a
Jeff Ding <jeffding@Jeffs-MacBook-Pro.local>
2022-04-06 12:19:12 -0400

    new endpoint

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import flask
-from flask import request
+import json
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def home():
     arg = request.args['arg1']
     return 'hello world'
+
+
+@app.route('/getName')
+def index():
+    return jsonify({'name': 'alice',
+                    'email': 'alice@outlook.com'})
