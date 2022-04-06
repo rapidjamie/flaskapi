65da2d8ab932964885b3243412266f288eccb204
Jeff Ding <jeffding@Jeffs-MacBook-Pro.local>
2022-04-06 13:47:27 -0400

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 
 from marshmallow import Schema, fields
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder='swagger/templates')
 
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
