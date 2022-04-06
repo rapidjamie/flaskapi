f59996f5eb4708431dd7dd04d74ccb10b9f3a581
Jeff Ding <jeffding@Jeffs-MacBook-Pro.local>
2022-04-06 12:28:35 -0400

    new endpoints

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 import flask
-import json
 from flask import request, jsonify
+import re
 
 app = flask.Flask(__name__)
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 
 @app.route('/', methods=['GET'])
 def home():
-    arg = request.args['arg1']
     return 'hello world'
 
 
-@app.route('/getName')
+@app.route('/getAllNames')
 def index():
     return jsonify({'name': 'alice',
                     'email': 'alice@outlook.com'})
+
+
+@app.route('/validateEmail/<email>')
+def email(email):
+    return isValid(email)
+
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+        return "Valid email"
+    else:
+        print "Invalid email"
